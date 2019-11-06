3b9a44d99e3411905dc13c8458058a68b6436dc8
simeon <a@bysimeon.com>
2019-11-05 19:26:59 -0500

    about change

--- src/components/about.js
+++ src/components/about.js
@@ -10,7 +10,7 @@ const About = ({data}) => {
                     simeon
                 </h1>
                 <p className="large-text inline">
-                    {" "}is a full stack developer interested in the ways that internet use can change the ways that we think, percieve, behave, identify and learn.
+                    {" "}is a full stack developer* interested in the ways that internet use can change the ways that we think, percieve, behave, identify and learn.
                 </p>
             </div>
             <button id="aboutToggle">
