5aa232628903b321e8caf524886bf0b35faa0ad9
simeon <a@bysimeon.com>
2019-11-07 18:47:55 -0500

    mdx

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { MDXProvider } from "@mdx-js/react"
 
@@ -17,22 +17,30 @@ const Project = ({ data }) => {
             <MDXProvider
                 components={{
                     h1: props => <h1 {...props} className="project__h1" />,
-                    h2: props => <h2 {...props} className="project__h2" />,
+                    h2: props => <p {...props} className="project__p" />,
                     h3: props => <h3 {...props} className="project__h3" />,
                     h4: props => <h4 {...props} className="project__h4" />,
                     h5: props => <h5 {...props} className="project__h5" />,
                     h6: props => <h6 {...props} className="project__h6" />,
-                    p: props => <p {...props} className="project__p" />,
                     a: props => <a {...props} className="project__a" />,
                     img: props => <img {...props} className="project__img" />,
                 }}
             >
+                
                 <section id={frontmatter.title} className="project__container">
-                    <h1 className="project__h1">{frontmatter.title}</h1>
-                    <p> {frontmatter.role} </p>
-                    <a href={frontmatter.website}>visit</a>
-                    
-
+                    {/* <Link className="project__back" to="/">
+                        <svg class="">
+                            <path class="shape-outline outer-color" d="M8.0008001,17.2999992c-0.4000001,0.3999996-0.4000001,1,0,1.3999996l15.7991991,15.7992001								c0.3999996,0.4000015,1,0.4000015,1.3999996,0l2.6992016-2.6991997c0.3999996-0.3999996,0.3999996-1,0-1.3999996								L16.2992001,18.7000008c-0.4000006-0.3999996-0.4000006-1,0-1.3999996L27.8992004,5.6999998								c0.3999996-0.4000001,0.3999996-1,0-1.4000001l-2.6992016-2.6992002c-0.3999996-0.4-1-0.4-1.3999996,0L8.0008001,17.2999992z"/>
+                            <path class="shape-shape inner-color" d="M9.6999998,17.6000004c-0.1999998,0.2000008-0.1999998,0.5,0,0.7000008l14.500001,14.5000019								c0.2000008,0.2000008,0.5,0.2000008,0.7000008,0l1.3999996-1.3999996c0.2000008-0.2000008,0.2000008-0.5,0-0.7000008								L13.8999996,18.3999996c-0.1999998-0.2000008-0.1999998-0.5,0-0.7000008L26.2999992,5.3000002								c0.2000008-0.1999998,0.2000008-0.5,0-0.6999998l-1.3999996-1.4000001c-0.2000008-0.2-0.5-0.2-0.7000008,0L9.6999998,17.6000004z"/>
+                        </svg>
+                    </Link> */}
+                    <div className="project__header">
+                        <h1 className="project__h1">{frontmatter.title}</h1>
+                        <a href={frontmatter.website}>visit</a>
+                        &nbsp;&nbsp;&nbsp;
+                        {frontmatter.github ? (<a href={frontmatter.github}>github</a>) : (<div></div>)}
+                    </div>
+                    {/* <p> {frontmatter.role} </p> */}
                     <MDXRenderer>{body}</MDXRenderer>
                 </section>
             </MDXProvider>
