d5f4599b173029ae4f31a032d6e4087b0eabdad2
simeon <a@bysimeon.com>
2019-11-08 12:07:25 -0500

    ***

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
                     {
                         resolve: "gatsby-remark-images",
                         options: {
-                            maxWidth: 1200,
+                            maxWidth: 1500,
                             linkImagesToOriginal: false
                         },
                     },
--- src/components/preview.js
+++ src/components/preview.js
@@ -5,21 +5,42 @@ import Img from "gatsby-image"
 const ProjectPreview = ({data}) => {
     const project = data.frontmatter
     return (
-        <Link to={project.path}>
-            <div className="browser">
-                <div className="browser-tab">
-                    <div className="browser-tab-button"></div>
-                    <div className="browser-tab-button"></div>
-                    <div className="browser-tab-button"></div>
-                </div>
-                <div className="browser-content">
-                    <Img className="browser-content-image" fluid={project.featureImage.childImageSharp.fluid} />
-                    <div className="browser-content-title">
-                        {project.title}
+        project.tags !== "unreleased" ? (
+            <Link to={project.path}>
+                <div className="browser">
+                    <div className="browser-tab">
+                        <div className="browser-tab-button"></div>
+                        <div className="browser-tab-button"></div>
+                        <div className="browser-tab-button"></div>
+                    </div>
+                    <div className="browser-content">
+                        <Img className="browser-content-image" fluid={project.featureImage.childImageSharp.fluid} />
+                        <div className="browser-content-title">
+                            {project.title}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Link >
+            </Link >
+        ) : (
+            <Link to={project.path}>
+                <div className="browser">
+                    <div className="browser-tab">
+                        <div className="browser-tab-button"></div>
+                        <div className="browser-tab-button"></div>
+                        <div className="browser-tab-button"></div>
+                    </div>
+                    <div className="browser-content">
+                        {/* <Img className="browser-content-image" fluid={project.featureImage.childImageSharp.fluid} /> */}
+                        <div className="browser-content-title">
+                            {project.title}
+                        </div>
+                        {/* <div className="browser-content-link">
+                                unreleased
+                        </div> */}
+                    </div>
+                </div>
+            </Link >
+        )
     )
 }
 
